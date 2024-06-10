9d3c974e31eead70510d6e58b3263527caff1dfa
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-10 05:52:48 +0000

    fix error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,14 +6,14 @@ from collections import deque
 
 def canUnlockAll(boxes):
     """Solve Lockbox using Breadth-First Search"""
-    n = len(lockboxes)
+    n = len(boxes)
     visited = [False] * n
     visited[0] = True
     queue = deque([0])
 
     while queue:
         current = queue.popleft()
-        for key in lockboxes[current]:
+        for key in boxes[current]:
             if not visited[key]:
                 visited[key] = True
                 queue.append(key)
