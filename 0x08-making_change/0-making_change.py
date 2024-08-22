69513642be878876fdab1732899f0c374d615d67
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-08-22 22:21:16 +0000

    implement making change algorithm

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount total.
+
+    Args:
+        coins (list): The values of the coins in your possession.
+        total (int): The total amount to meet with coins.
+
+    Returns:
+        int: Fewest number of coins needed to meet the total, or -1
+        if not possible.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
